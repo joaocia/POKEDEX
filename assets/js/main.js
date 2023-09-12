f8485a2c80d82ea87ebdf773b1a7ee0f326598d1
joaocia <joaocia@gmail.com>
2023-09-12 10:31:23 -0300

    12.09_commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
 function convertPokemonToLi(pokemon) {
+
   return `
         <li class="pokemon">
             <span class="number">#001</span>
@@ -21,16 +19,25 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons [i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-        }
-    })
-    .catch((error) => console.error(error))
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
+    
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        
+        
+        
+    
 
 
